03da44190f2e9dfb3590a7189de2cbc88b5e5615
Muhammad Subhan Khan <subhanism@outlook.com>
2022-07-16 12:30:59 +0500

    created helper to generate token

--- src/helpers/generate-token.js
+++ src/helpers/generate-token.js
@@ -1,62 +1,49 @@
 /* eslint-disable indent */
 import dayjs from 'dayjs'
 import jwt from 'jsonwebtoken'
-import { AuthToken } from '../models/AuthToken'
-import { RefreshToken } from '../models/RefreshToken'
+import AuthToken from '../models/AuthToken'
+import RefreshToken from '../models/RefreshToken'
 import dotenv from 'dotenv'
 dotenv.config()
-export const generateToken = async (user, scopes = [], rememberMe = false) => {
+export const generateToken = async (user) => {
     try {
         // number of seconds in a day
         const secondsInADay = 24 * 60 * 60
-        const tokenExpirationDate = rememberMe
-            ? dayjs().add(process.env.TOKEN_EXPIRATION_TIME_REMEMBER_ME, 'day')
-            : dayjs().add(process.env.TOKEN_EXPIRATION, 'day')
+        const tokenExpirationDate = dayjs().add(1, 'day')
 
         const accessTokenInstance = new AuthToken()
         accessTokenInstance.userId = user._id
-        accessTokenInstance.scopes = scopes
         accessTokenInstance.revokedAt = null
         accessTokenInstance.expiresAt = tokenExpirationDate
-        // eslint-disable-next-line no-console
-        console.log('--->', accessTokenInstance, typeof tokenExpirationDate)
         if (await accessTokenInstance.save()) {
-            // eslint-disable-next-line no-console
-            console.log('accessTokenInstance,', accessTokenInstance)
             const refreshTokenInstance = new RefreshToken()
             refreshTokenInstance.accessTokenId = accessTokenInstance._id
             refreshTokenInstance.expiresAt = tokenExpirationDate
+            const refresh = await refreshTokenInstance.save()
 
-            if (await refreshTokenInstance.save()) {
+            if (refresh) {
                 const accessToken = jwt.sign(
                     {
                         accessTokenId: accessTokenInstance._id,
                         userId: user._id,
-                        scopes: accessTokenInstance.scopes,
                     },
-                    process.env.ClIENT_SECRET,
+                    process.env.CLIENT_SECRET,
                     {
-                        expiresIn: rememberMe
-                            ? process.env.TOKEN_EXPIRATION_TIME_REMEMBER_ME *
-                              secondsInADay
-                            : process.env.TOKEN_EXPIRATION_TIME * secondsInADay,
+                        expiresIn: secondsInADay,
                     }
                 )
                 const refreshToken = jwt.sign(
                     {
-                        refreshTokenId: refreshTokenInstance._id,
+                        refreshTokenId: refresh._id,
                         userId: user._id,
-                        scopes: accessTokenInstance.scopes,
                     },
-                    process.env.ClIENT_SECRET,
+                    process.env.CLIENT_SECRET,
                     {
-                        expiresIn: rememberMe
-                            ? process.env.TOKEN_EXPIRATION_TIME_REMEMBER_ME *
-                              secondsInADay
-                            : process.env.TOKEN_EXPIRATION_TIME * secondsInADay,
+                        expiresIn: secondsInADay,
                     }
                 )
-
+                // eslint-disable-next-line no-console
+                console.log('acces', accessToken, 'refresh', refreshToken)
                 return { accessToken, refreshToken, tokenExpirationDate }
             }
         }
